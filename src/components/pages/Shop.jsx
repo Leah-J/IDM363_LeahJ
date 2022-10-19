ab0b7de4299e191a000de95c0b57af27a29a2ef9
Leah Jorge <leahjorge14@gmail.com>
2022-10-18 20:00:44 -0400

    add responsive grid to shop page :)

--- src/components/pages/Shop.jsx
+++ src/components/pages/Shop.jsx
@@ -2,20 +2,23 @@ import Layout from "../layouts/Layout";
 import "../../index.css";
 import ShopItemCard from "../shopitems/ShopItemCard";
 import Sidebar from "../shopitems/Sidebar";
+import { Row, Col } from "react-bootstrap";
 
 const Shop = ({ children }) => {
   const customData = require("../../data/decks.json");
 
-  let decks = customData.decks;
+  let decks = customData.decks; //Array of Decks from JSON
 
   const cards = decks.map(
     (decks = ({ name, description, price, source }) => (
-      <ShopItemCard
-        name={name}
-        desc={description}
-        price={price}
-        source={source}
-      />
+      <Col>
+        <ShopItemCard
+          name={name}
+          desc={description}
+          price={price}
+          source={source}
+        />
+      </Col>
     ))
   );
 
@@ -24,7 +27,7 @@ const Shop = ({ children }) => {
       <Layout>
         <Sidebar />
         <div className="shopcontent">
-          {cards}
+          <Row lg={3}>{cards}</Row>
           {children}
         </div>
       </Layout>
